c1556b5e17f7bbd0037f207467d5f1bf1ac076d5
Oskar Eriksson <oskeriks@chalmers.se>
2026-04-09 15:41:05 +0200

    Built some more in the Card-file. Created a separate file for the texasHoldEm game

---
+++ app/TexasLogic.hs
@@ -0,0 +1,10 @@
+
+module TexasLogic where
+
+import Cards
+
+
+
+
+
+
--- app/cards.hs
+++ app/cards.hs
@@ -1,16 +1,51 @@
 {- Card related data-types -}
 
-data Suit = Hearts | Spades | Diamonds | Clubs
-  deriving (Show)
 
-data Rank = Ace | King | Queen | Jack | Num Int
-  deriving (Show)
+-----------------------
+------For testing -----
+hand1 :: Hand
+hand1 = [ Card (Num 2) Hearts, Card Jack Spades]
+-----------------------
 
-data Card = Card Rank Suit
 
+ -- | All different suits
+data Suit = Hearts | Spades | Diamonds | Clubs deriving (Show, Eq)
+
+-- | All different ranks
+data Rank =  Num Int | Jack | Queen | King | Ace deriving (Show, Eq)
+
+ -- | Card has an rank and a suit
+data Card = Card Rank Suit deriving (Eq)
+
+ -- | to show the cards in a nice way
 instance Show Card where
-  show (Card r s) = show r ++ " of " ++ show s
+  show (Card r s) = " " ++ formatedRank r ++ " of " ++ formatedSuit s
 
+formatedRank :: Rank -> String
+formatedRank (Num n) = show n
+formatedRank rank    = show rank
 
-hand = [Card]
+formatedSuit :: Suit -> String
+formatedSuit s 
+            | s == Spades = "\9824"
+            | s == Hearts = "\9829"
+            | s == Diamonds = "\9830"
+            | s == Clubs = "\9827"
 
+ -- | Hand is a list of cards
+type Hand = [Card]
+
+-- | Deck is a list of cards
+type Deck = [Card]
+
+-- | Extract the rank of a card
+rank :: Card -> Rank
+rank (Card r _) = r
+
+-- | Extract the suit of a card
+suit :: Card -> Suit
+suit (Card _ s) = s
+
+-- | Extract the size of an hand
+size :: Hand -> Int
+size hand = length hand
