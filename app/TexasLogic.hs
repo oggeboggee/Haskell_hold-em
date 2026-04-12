ebf9774dc8d01d1afff6ec32d2efaea6e5d9cf57
linuxcodes <jmhd58nr8n@privaterelay.appleid.com>
2026-04-12 21:48:30 +0200

    new branch game-state

--- app/TexasLogic.hs
+++ app/TexasLogic.hs
@@ -29,9 +29,9 @@ data Player = Player
             {
             name :: String,
             hand :: Hand,
-            chips :: Chip,
-            folded :: Bool,
-            blind :: Blind
+            --chips :: Chip,
+            --folded :: Bool,
+            --blind :: Blind
             }
 
 -- | The table represent the gamestate
@@ -39,9 +39,10 @@ data Table = Table
             {
                 players :: [Player],
                 deck :: Deck,
+                board :: CommunityCard,
                 phase :: GamePhase,
-                pot :: Pot,
-                dealPosition :: Int
+                --pot :: Pot,
+                --dealPosition :: Int
             }
 
 ------------------------------
@@ -55,6 +56,50 @@ nextPhase p = case p of
     River       -> Showdown
     Showdown    -> Dealhands
 
+-- | Updating what happens during each phase (dealing cards, showing community cards, etc.) and moves phase to next phase
+gameStep :: Table -> Table
+gameStep table = case phase table of
+    DealHands ->
+        let (hand1, deck1) = dealCards (deck t) 2
+            (hand2, deck2) = dealCards deck1 2
 
+            [player1, player2] = players table
 
+            player1' = player1 {hand = hand1}
+            player2' = player2 {hand = hand2}
+        in table { players = [player', player2'],
+                   deck = deck2,
+                   phase = nextPhase (phase table )
+               
+                 }
+    PreFlop -> t {
+        phase = nextPhase (phase table)
+    }
+        -- First betting rounds, player can bet, fold, call, raise. (logic we don't have yet)
+    Flop ->
+        let (flop, newDeck ) = dealCards (deck table) 3
+        in table { board = flop,
+                   deck = newDeck,
+                   phase = nextPhase (phase table)
+                 }
 
+    Turn ->
+       let (turn, newDeck) = dealCards (deck table) 1
+       in table { board = board table ++ turn,
+                  deck = newDeck
+                  phase = nextPhase (phase table)
+                }
+
+    River ->
+       let (river, newDeck) = dealCards (deck table) 1
+       in table { board = board table ++ turn,
+                  deck = newDeck
+                  phase = nextPhase (phase table)
+                }
+
+    Showdown -> table -- Here we have more betting logic, showing hands to all players?
+
+    --test text
+ 
+
+              
