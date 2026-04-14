3100ec1d07aab4e77c5414767039366989cb35fd
axelnygren <s.axel.nygren@gmail.com>
2026-04-14 17:03:33 +0200

    git stuff, hopefully works this time..

--- app/TexasLogic.hs
+++ app/TexasLogic.hs
@@ -1,4 +1,3 @@
-
 module App.TexasLogic where
 
 import App.Cards
--- app/cards.hs
+++ app/cards.hs
@@ -97,3 +97,54 @@ size hand = length hand
 --   might be easier to implement Straight in combination. It might also be useful if we want to
 --   extend with other card-games later (the game "Casino" for example when the values is vital
 --   for the game).
+
+----------------------------------------------
+-- | Creates a new deck with 52 cards
+fullDeck :: Deck
+fullDeck = [Card r s | r <- allRank, s <- allSuit]
+  where
+    allRank = [Num r | r <- [2..10]] ++ [Jack, Queen, King, Ace]
+    allSuit = [Clubs, Diamonds, Hearts, Spades]
+
+
+-- | If deck is not empty, draw a card from a deck and remove it from the deck
+drawCard :: Deck -> Maybe (Card, Deck)
+drawCard [] = Nothing
+drawCard (x:xs) = Just (x, xs)
+
+
+----------------------------------------------
+-- | Removes the first card that matches a given card from the deck.
+removeCard:: Card -> Deck -> Deck
+removeCard _ [] = []
+removeCard x (y:ys)
+  | x == y    = ys
+  | otherwise = y : removeCard x ys
+
+-- |Use a double between 0-1 to find a index 
+pick :: Double -> Deck -> Card
+pick x deck = deck !! round ((fromIntegral (length deck - 1)) * x)
+
+-- | Pick a random card using a double(for index) and put it in a new deck
+shuffle :: [Double] -> Deck -> Deck
+shuffle _ [] = []
+shuffle (x:xs) deck = card : shuffle xs deck'
+  where
+    card  = pick x deck
+    deck' = removeCard card deck
+
+-- | Generate a list with random doubles
+randomDoubles :: Int -> StdGen -> ([Double], StdGen)
+randomDoubles 0 gen = ([], gen)
+randomDoubles n gen = ((x:xs), gen2)
+  where
+    (x, gen1)  = randomR (0.0,1.0) gen
+    (xs, gen2) = randomDoubles (n-1) gen1
+
+-- | Create a new random genarator and shuffle a full deck
+runShuffle :: IO Deck
+runShuffle = do
+  gen <- newStdGen
+  let (doubles, _) = randomDoubles 52 gen
+  return (shuffle doubles fullDeck)
+
