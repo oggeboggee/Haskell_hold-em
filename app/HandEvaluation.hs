ec37648ab00c7d97e9065bc34ecd3e6767394e54
Oskar Eriksson <oskeriks@chalmers.se>
2026-04-15 19:07:09 +0200

    working on combinations

--- app/HandEvaluation.hs
+++ app/HandEvaluation.hs
@@ -5,52 +5,68 @@ module HandEvaluation where
 import Cards
 import Data.List
 import Data.Maybe
+import Data.Function (on)
 
 
 ----------------------------
 -------- Test Hands --------
 ----------------------------
+
 hand0 :: Hand
-hand0 = [Card (Num 4) Hearts, Card (Num 4) Spades ]
+hand0 = [Card Two Hearts, Card Four Spades ]
 
 hand1 :: Hand
-hand1 = [ Card (Num 2) Hearts, Card Jack Spades]
+hand1 = [ Card Two Hearts, Card Jack Spades]
 
 hand2 :: Hand
-hand2 = [ Card (Num 2) Hearts, Card Jack Spades, Card (Num 5) Clubs]
+hand2 = [ Card Two Hearts, Card Jack Spades, Card Five Clubs]
 
 
 hand3 :: Hand
-hand3 = [ Card (Num 2) Spades, Card (Num 5) Clubs, Card (Num 2) Clubs, Card Jack Spades ]
+hand3 = [ Card Two Spades, Card Five Clubs, Card Two Clubs, Card Jack Spades ]
 
 -- | sorted
 hand4 :: Hand
-hand4 = [ Card (Num 2) Spades,  Card (Num 2) Clubs, Card (Num 2) Hearts, Card (Num 5) Clubs, Card Jack Spades ]
+hand4 = [ Card Two Spades,  Card Two Clubs, Card Two Hearts, Card Five Clubs, Card Jack Spades ]
 
-hand5 :: Hand
-hand5 = [Card (Num 10) Hearts , Card Jack Hearts, Card Queen Hearts, Card King Hearts, Card Ace Hearts]
+hand5 :: Hand --Flush
+hand5 = [Card Ten Hearts , Card Jack Hearts, Card Queen Hearts, Card King Hearts, Card Ace Hearts]
 
-hand6 :: Hand
-hand6 = [Card Queen Hearts, Card Queen Spades, Card King Hearts, Card King Spades, Card Ace Hearts ]
+hand6 :: Hand --Flush
+hand6 = [Card Three Hearts, Card Ten Hearts , Card Jack Spades, Card Queen Hearts, Card King Hearts, Card Ace Hearts]
 
-hand7 :: Hand --one pair
-hand7 = [Card Jack Hearts, Card Jack Spades, Card Queen Hearts, Card King Spades, Card Ace Hearts ]
+hand7 :: Hand
+hand7 = [Card Queen Hearts, Card Queen Spades, Card King Hearts, Card King Spades, Card Ace Hearts ]
 
-hand8 :: Hand --two pair
-hand8 = [Card Jack Hearts, Card Jack Spades, Card King Hearts, Card King Spades, Card Ace Hearts]
+hand8 :: Hand --one pair
+hand8 = [Card Jack Hearts, Card Jack Spades, Card Queen Hearts, Card King Spades, Card Ace Hearts ]
 
 hand9 :: Hand --two pair
-hand9 = [Card Jack Hearts, Card Jack Spades, Card Queen Hearts, Card King Spades, Card King Hearts]
+hand9 = [Card Jack Hearts, Card Jack Spades, Card King Hearts, Card King Spades, Card Ace Hearts]
 
-hand10 :: Hand --three of a Kind (x1)
-hand10 = [Card Jack Clubs, Card Jack Spades, Card Jack Hearts, Card King Spades, Card King Hearts, Card Ace Clubs]
+hand10 :: Hand --two pair
+hand10 = [Card Jack Hearts, Card Jack Spades, Card Queen Hearts, Card King Spades, Card King Hearts]
 
-
-hand11 :: Hand --three of a Kind (x2)
-hand11 = [Card Jack Clubs, Card Jack Spades, Card Jack Hearts, Card King Spades, Card King Hearts, Card King Clubs]
-
+hand11 :: Hand --three of a Kind (x1)
+hand11 = [Card Jack Clubs, Card Jack Spades, Card Jack Hearts, Card King Spades, Card King Hearts, Card Ace Clubs]
 
 
+hand12 :: Hand --three of a Kind (x2)
+hand12 = [Card Jack Clubs, Card Jack Spades, Card Jack Hearts, Card King Spades, Card King Hearts, Card King Clubs]
+
+
+hand13 :: Hand --Full House (x2)
+hand13 = [Card Jack Clubs, Card Jack Spades, Card Jack Hearts, Card King Spades, Card King Hearts, Card Ace Clubs]
+
+
+hand14 :: Hand --Full House (x2)
+hand14 = [Card Jack Clubs, Card Jack Spades, Card King Hearts, Card King Spades, Card King Hearts, Card Ace Clubs]
+
+hand15 :: Hand -- Quads
+hand15 = [Card Two Hearts, Card Two Spades, Card Two Diamonds, Card Two Clubs, Card King Hearts, Card Ace Diamonds, Card Ace Hearts]
+
+hand16 :: Hand -- Quads
+hand16 = [Card Two Hearts, Card Two Spades, Card Two Diamonds, Card Two Clubs, Card King Hearts, Card King Spades, Card King Clubs, Card King Diamonds]
 ----------------------------
 ----------------------------
 
@@ -58,6 +74,18 @@ hand11 = [Card Jack Clubs, Card Jack Spades, Card Jack Hearts, Card King Spades,
 -- | @output list with the cards ranks, sorted
 sortRankHandList :: [Card] -> [Rank]
 sortRankHandList hand = sort [ rank x | x <- hand]
+
+
+-- | @input Hand
+-- | @output list with the cards suits, sorted
+sortSuitHandList :: Hand -> [Suit]
+sortSuitHandList hand = sort [ suit x | x <- hand]
+
+
+
+
+tupleList :: [Card] -> [(Suit, Rank)]
+tupleList cards = [ (suit x, rank x) | x <- cards]
 
 
 -- | Bool       = if the combination exist in the hand
@@ -173,6 +201,144 @@ indexOfThreeOfAKind cards = [firstIndex, firstIndex +1, firstIndex+2]
           firstIndex = sum (drop 1 (dropWhile (/=3) (reverse mpHand)))
 
 -------------- End three of a kind --------------
+-------------------- Straight -------------------
+{-
+hasStraight :: [Card] -> (Bool, Maybe Rank, Maybe [Int])
+hasStraight cards
+     | hasStraightBool cards = (True, Just (rankStraight cards), Just (indexStraight cards))
+     | otherwise            = (False, Nothing, Nothing)
+
+
+hasStraight :: [Card] -> Bool
+hasStraight cards
+    | length noDuplicates < 5 = False
+    |  
+    where
+        rankHand     = sortRankHandList cards
+        noDuplicates = nub rankHand
+-}
+
+------------------ End Straight -----------------
+---------------------- Flush --------------------
+
+hasFlush :: [Card] -> (Bool, Maybe Rank, Maybe [Int])
+hasFlush cards
+     | hasFlushBool cards = (True, Nothing, Just (indexOfFlush cards))
+     | otherwise          = (False, Nothing, Nothing)
+
+hasFlushBool :: [Card] -> Bool
+hasFlushBool cards
+          | flush /= 0 = True
+          | otherwise   = False
+     where
+          grHand = group (sortSuitHandList cards)        
+          mpHand = map (length) grHand
+          flush  = length (filter (>=5) mpHand)
+
+
+indexOfFlush :: [Card] -> [Int]
+indexOfFlush cards = getIndex cards sortedAfterRank
+     where
+          sortedTupleList   = sort (tupleList cards)
+          groupedBySuit     = groupBy ((==) `on` fst) (sortOn fst sortedTupleList)
+          flushGroups       = filter ((>=5) . length) groupedBySuit
+          highestRankOfSuit = (take 5 (reverse (concat flushGroups)))
+          sortedAfterRank   = switch highestRankOfSuit
+          
+
+
+switch :: [(Suit, Rank)] -> [Card]
+switch []           = []
+switch ((s1,r1):xs) =  (Card r1 s1) : switch xs
+
+
+getIndex :: [Card] -> [Card] -> [Int]
+getIndex xs ys = [ i | (i, x) <- zip [0..] xs, x `elem` ys ]
+
+-------------------- End Flush ------------------
+-------------------- Full House -----------------
+
+
+hasFullHouse :: [Card] -> (Bool, [Maybe Rank], Maybe [Int])
+hasFullHouse cards
+     | hasFullHouseBool cards = (True,
+                                [Just (rankThreeOfAKind cards), Just bestRankPair],
+                                 Just ([bestPairIndex cards,
+                                       (bestPairIndex cards + 1)] ++
+                                       indexOfThreeOfAKind cards)  )
+
+     | otherwise            = (False, [Nothing], Nothing)
+     where
+          bestRankPair = rank (cards!!(bestPairIndex cards))
+
+-- | Helpers
+hasFullHouseBool :: [Card] -> Bool
+hasFullHouseBool cards
+     | (tripples >= 1) && (pairs >= 1) = True
+     | (tripples >=2 ) = True
+     | otherwise  = False
+     where
+          grHand   = group (sortRankHandList cards)        
+          mpHand   = map (length) grHand
+          pairs    = length (filter (==2) mpHand)
+          tripples = length (filter (==3) mpHand) 
+
+
+
+bestPairIndex :: [Card] -> Int
+bestPairIndex cards = sum (drop 1 (dropWhile (/=2) (reverse mpHand)))
+        where
+          grHand = group (sortRankHandList cards)        
+          mpHand = map (length) grHand
+          
+
+------------------ End Full House ---------------
+---------------------- Quads --------------------
+
+hasQuads :: [Card] -> (Bool, Maybe Rank, Maybe [Int])
+hasQuads cards
+     | hasQuadsBool cards = (True, Just (rankQuads cards), Just (indexOfQuads cards))
+     | otherwise            = (False, Nothing, Nothing)
+
+
+hasQuadsBool :: [Card] -> Bool
+hasQuadsBool cards
+     | quads >= 1 = True
+     | otherwise  = False
+     where
+          grHand = group (sortRankHandList cards)        
+          mpHand = map (length) grHand
+          quads  = length (filter (==4) mpHand)
+
+
+rankQuads :: [Card] -> Rank
+rankQuads cards = rank (cards!!index)
+     where
+          grHand = group (sortRankHandList cards)        
+          mpHand = map (length) grHand
+          index = sum (dropWhile (/=4) (reverse mpHand)) -1
+
+indexOfQuads :: [Card] -> [Int]
+indexOfQuads cards = [firstIndex, firstIndex +1, firstIndex+2, firstIndex+3]
+        where
+          grHand = group (sortRankHandList cards)        
+          mpHand = map (length) grHand
+          firstIndex = sum (drop 1 (dropWhile (/=4) (reverse mpHand)))
+
+-------------------- End Quads ------------------
+----------------- Straight Flush ----------------
+
+
+hasStraightFlush :: [Card] -> (Bool, Maybe Rank, Maybe [Int])
+hasStraightFlush cards = undefined
+
+--------------- End Straight Flush --------------
+
+
+
+
+
+
 
 
 
--- app/cards.hs
+++ app/cards.hs
@@ -4,10 +4,53 @@ module Cards where
 
 
  -- | All different suits
-data Suit = Hearts | Spades | Diamonds | Clubs deriving (Show, Eq, Ord)
+data Suit = Hearts | Spades | Diamonds | Clubs deriving (Eq, Ord)
 
 -- | All different ranks
-data Rank =  Num Int | Jack | Queen | King | Ace deriving (Show, Eq, Ord)
+
+data Rank =   Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace 
+  deriving (Eq, Ord)
+
+instance Show Rank where
+  show r = case r of
+    Two -> "2"
+    Three -> "3"
+    Four -> "4"
+    Five -> "5"
+    Six -> "6"
+    Seven -> "7"
+    Eight -> "8"
+    Nine -> "9"
+    Ten -> "10"
+    Jack  -> "J"
+    Queen -> "Q"
+    King  -> "K"
+    Ace   -> "A"
+
+rankValue :: Rank -> Int
+rankValue r = case r of
+  Two   -> 2
+  Three -> 3
+  Four  -> 4
+  Five  -> 5
+  Six   -> 6
+  Seven -> 7
+  Eight -> 8
+  Nine  -> 9
+  Ten   -> 10
+  Jack  -> 11
+  Queen -> 12
+  King  -> 13
+  Ace   -> 14
+
+
+instance Show Suit where
+  show s = case s of
+    Spades -> "♠"
+    Hearts -> "♥"
+    Diamonds -> "♦"
+    Clubs -> "♣"
+
 
  -- | Card has an rank and a suit
 data Card = Card Rank Suit deriving (Eq, Ord)
@@ -29,11 +72,12 @@ data Combination =  HighCard
 
  -- | to show the cards in a nice way
 instance Show Card where
-  show (Card r s) = " " ++ formatedRank r ++ " of " ++ formatedSuit s
-
+  show (Card r s) = show r ++ show s
+{-
 formatedRank :: Rank -> String
 formatedRank (Num n) = show n
 formatedRank rank    = show rank
+
 
 formatedSuit :: Suit -> String
 formatedSuit s 
@@ -41,7 +85,7 @@ formatedSuit s
             | s == Hearts = "\9829"
             | s == Diamonds = "\9830"
             | s == Clubs = "\9827"
-
+-}
  -- | Hand is a list of cards
 type Hand = [Card]
 
