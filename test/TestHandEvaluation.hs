8ac28afd3cdd9838ae223ba7c541d242691ec913
Oskar Eriksson <oskeriks@chalmers.se>
2026-05-10 14:40:11 +0200

    tests for removeCards

--- test/TestHandEvaluation.hs
+++ test/TestHandEvaluation.hs
@@ -1,5 +1,7 @@
 module TestHandEvaluation where
 
+import Data.Maybe
+import Data.Function (on)
 
 import Types
 import Cards
@@ -34,12 +36,38 @@ propAverage a b = average' a b == a/2 + b/2
 ----------------------------------------
 ----------------------------------------
 
+ -- | properties for function removeCards
+propRemCardsAll :: [Card] -> [Card] -> Bool
+propRemCardsAll cards _ = removeCards cards cards == []
+
+propRemCardsNone :: [Card] -> [Card] -> Bool
+propRemCardsNone cards _ = removeCards [] cards == cards
+
 propRemCards :: [Card] -> [Card] -> Bool
-propRemCards cards _ = removeCards cards cards == []
+propRemCards cards c2 = (propRemCardsAll cards c2) && (propRemCardsNone cards c2)
+
+
+-- | properties for function tripsTwoPairHigh
+propTrips :: [Card] -> Bool
+propTrips cards = undefined
+--  where
+--    lengthList = map length (groupBy ((==) `on` rank) cards)
+
+
+-- What do we want to test?
+-- the function does only give nothing on a empty list
+-- the function gives combination threeOfAKind if we have 3 of same rank
+-- the function gives combination twoPair if we have 2 of same rank
+-- the function gives combaination HighCard with the highest possible card first in the return-hand
+
+
+-- maybe False (==1) Nothing (how to use maybe)
+
+
+
 
 -- TODO
 
--- function removeCards
 -- function combinationRoot
 -- function straightFlush
 -- function straight
