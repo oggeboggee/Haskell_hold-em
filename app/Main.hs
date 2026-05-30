eacf49c21111961a27166ea4e360782fa98a4f82
linuxcodes <jmhd58nr8n@privaterelay.appleid.com>
2026-05-30 16:05:50 +0200

    removed .txt files, updated license file, added comment to main

--- app/Main.hs
+++ app/Main.hs
@@ -10,6 +10,8 @@ import qualified Network.WebSockets as WS
 import Control.Concurrent.STM
 import Server.ServerTypes
 
+-- | Entry point for the poker WebSocket server.
+--   Initializes the game state and starts the server on port 9160.
 main :: IO ()
 main = do
 	let initialTable = Table
@@ -26,5 +28,7 @@ main = do
         }         
     
 	putStrLn "Poker server running on ws://localhost:9160"
+
 	stateVar <- newTVarIO (initServerState initialTable)
+    
 	WS.runServer "127.0.0.1" 9160 (server stateVar)
